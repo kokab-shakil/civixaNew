a20465f542d792ad0165094d07d4870e5c3fbe05
Jawadcivixa <jawad@vortechs.io>
2021-08-17 12:38:30 +0500

    service page done

--- src/pages/Service/components/Classification.js
+++ src/pages/Service/components/Classification.js
@@ -6,40 +6,44 @@ import Construction from "../../../images/construction.svg";
 import tick from "../../../images/tick.svg";
 
 const data = [
-  "Custom Pricing for High Volume",
-  "Dedicated Account Managers",
-  "24/7 Support",
-  "Custom Workflows",
+	"Custom Pricing for High Volume",
+	"Dedicated Account Managers",
+	"24/7 Support",
+	"Custom Workflows",
 ];
 
 export const Classification = ({ classes }) => {
-  return (
-    <Row className={classes}>
-      <Col xs={12} md={4} className="order-lg-last">
-        <Heading
-          iconBefore
-          icon={Construction}
-          altText=""
-          headingClass="font-weight-normal"
-          HeadingText="Classification"
-          para
-          paraText="You can also track people not following the rules instantly on your screens using your live CCTV footage."
-          paraClass=""
-        />
-        {data.map((item) => {
-          return (
-            <Heading
-              iconBefore
-              icon={tick}
-              headingClass="text-small font-weight-normal pl-3"
-              HeadingText={item}
-            />
-          );
-        })}
-      </Col>
-      <Col xs={12} md={8} className={"pt-4 order-lg-first"}>
-        <img src={copyright} className={"upper-grid"} alt="data-label-web" />
-      </Col>
-    </Row>
-  );
+	return (
+		<Row className={classes}>
+			<Col xs={12} md={4} className="order-lg-last">
+				<Heading
+					iconBefore
+					icon={Construction}
+					altText=""
+					headingClass="font-weight-normal"
+					HeadingText="Classification"
+					para
+					paraText="You can also track people not following the rules instantly on your screens using your live CCTV footage."
+					paraClass="my-3"
+				/>
+				{data.map((item) => {
+					return (
+						<Heading
+							iconBefore
+							icon={tick}
+							headingClass="text-small font-weight-normal pl-3"
+							HeadingText={item}
+						/>
+					);
+				})}
+			</Col>
+			<Col xs={12} md={8} className={"pt-4 order-lg-first"}>
+				<img
+					src={copyright}
+					className={"upper-grid"}
+					alt="data-label-web"
+				/>
+			</Col>
+		</Row>
+	);
 };
--- src/pages/Service/components/TextDetection.js
+++ src/pages/Service/components/TextDetection.js
@@ -6,70 +6,74 @@ import Construction from "../../../images/construction.svg";
 import tick from "../../../images/tick.svg";
 
 const data = [
-  {
-    id: 1,
-    itemText: "Custom Pricing for High Volume",
-    itemImage: TextDetectionImage,
-  },
-  {
-    id: 2,
-    itemText: "Dedicated Account Managers",
-    itemImage: TextDetectionImage,
-  },
-  {
-    id: 3,
-    itemText: "24/7 Support",
-    itemImage: TextDetectionImage,
-  },
-  {
-    id: 4,
-    itemText: "Custom Workflows",
-    itemImage: TextDetectionImage,
-  },
+	{
+		id: 1,
+		itemText: "Custom Pricing for High Volume",
+		itemImage: TextDetectionImage,
+	},
+	{
+		id: 2,
+		itemText: "Dedicated Account Managers",
+		itemImage: TextDetectionImage,
+	},
+	{
+		id: 3,
+		itemText: "24/7 Support",
+		itemImage: TextDetectionImage,
+	},
+	{
+		id: 4,
+		itemText: "Custom Workflows",
+		itemImage: TextDetectionImage,
+	},
 ];
 
 export const TextDetection = ({ classes }) => {
-  const [dataImage, setDataImage] = useState(TextDetectionImage);
+	const [dataImage, setDataImage] = useState(TextDetectionImage);
 
-  // const handleClick = (itemImage) => {
-  //   setDataImage(itemImage);
-  //   console.log(itemImage);
-  // };
+	// const handleClick = (itemImage) => {
+	//   setDataImage(itemImage);
+	//   console.log(itemImage);
+	// };
 
-  return (
-    <Row className={classes}>
-      <Col xs={12} md={4}>
-        <Heading
-          iconBefore
-          icon={Construction}
-          altText=""
-          headingClass="font-weight-normal"
-          HeadingText="TEXT DETECTION"
-          para
-          paraText="You can also track people not following the rules instantly on your screens using your live CCTV footage."
-          paraClass=""
-        />
-        {data.map((item) => {
-          return (
-            <div
-              onClick={() => {
-                setDataImage(item.itemImage);
-                console.log(item.id);
-              }}
-            >
-              <Heading
-                iconBefore
-                icon={tick}
-                headingClass="text-small font-weight-normal pl-3"
-                HeadingText={item.itemText}
-              />
-            </div>
-          );
-        })}
-      </Col>
-      <Col xs={12} md={8} className={"pt-4"}>
-        <img src={dataImage} className={"upper-grid"} alt="data-label-web" />
-      </Col>
-    </Row>
-  );
+	return (
+		<Row className={classes}>
+			<Col xs={12} md={4}>
+				<Heading
+					iconBefore
+					icon={Construction}
+					altText=""
+					headingClass="font-weight-normal"
+					HeadingText="TEXT DETECTION"
+					para
+					paraText="You can also track people not following the rules instantly on your screens using your live CCTV footage."
+					paraClass="my-3"
+				/>
+				{data.map((item) => {
+					return (
+						<div
+							onClick={() => {
+								setDataImage(item.itemImage);
+								console.log(item.id);
+							}}
+						>
+							<Heading
+								iconBefore
+								icon={tick}
+								headingClass="text-small font-weight-normal pl-3"
+								HeadingText={item.itemText}
+							/>
+						</div>
+					);
+				})}
+			</Col>
+			<Col xs={12} md={8} className={"pt-4"}>
+				<img
+					src={dataImage}
+					className={"upper-grid"}
+					alt="data-label-web"
+				/>
+			</Col>
+		</Row>
+	);
 };
